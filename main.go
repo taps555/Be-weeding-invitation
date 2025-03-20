ef43df17d7e79c5cf4c8b8901c8127cca2188738
trioadhi <trioadhipamungkas04@gmail.com>
2025-03-20 15:51:29 +0700

    fix frontend

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 
 	// Setup CORS
 	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://wedding-two-opal.vercel.app/"},
+		AllowedOrigins:   []string{"https://wedding-two-opal.vercel.app"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type"},
 		AllowCredentials: true,
